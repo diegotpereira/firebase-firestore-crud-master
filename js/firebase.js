15346e419d818a620d57cb4a053c66cb77816c55
diego <diegoteixeirapereira@hotmail.com>
2021-11-18 16:18:54 -0300

    criação do arquivo componente adicionar colaborador.

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB4WJ_nspMyFA_zsSZ9ky_SoQiS2QOp70I",
+    authDomain: "website-ecom-ff223.firebaseapp.com",
+    databaseURL: "https://website-ecom-ff223-default-rtdb.firebaseio.com",
+    projectId: "website-ecom-ff223",
+    storageBucket: "website-ecom-ff223.appspot.com",
+    messagingSenderId: "144186283636",
+    appId: "1:144186283636:web:d4ef8fe570ad91599f6321",
+    measurementId: "G-8BSZX9MJBH"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
